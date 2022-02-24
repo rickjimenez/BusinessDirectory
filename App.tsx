6852fc4be7799326c1c2549b29ee866e8b50f0b9
Rick <richjimenez@me.com>
2022-02-23 21:36:35 -0600

    init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+
+const { API_URL, API_KEY } = Constants.manifest?.extra || {};
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Open up App.tsx to start working on your app! {API_URL} {API_KEY}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ app.config.js
@@ -0,0 +1,33 @@
+const myValue = "Business Directory";
+
+export default {
+  name: myValue,
+  version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
+  icon: "./assets/icon.png",
+  orientation: "portrait",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+  extra: {
+    API_URL: "https://us4b9c5vv0.execute-api.us-east-1.amazonaws.com/prod",
+    API_KEY: "KlLL0NhwQN1Y0DyN99DbD7m06YVEiUP7aJv3Fdef",
+  },
+};
